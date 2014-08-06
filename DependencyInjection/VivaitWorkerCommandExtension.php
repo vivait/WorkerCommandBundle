3f956f74d2b8d71fd9432c8844f676b4cf58878e
Robin Cawser <robin@vivait.co.uk>
2014-08-06 14:08:28 +0100

    Rename extension

--- DependencyInjection/VivaitWorkerCommandExtension.php
+++ DependencyInjection/VivaitWorkerCommandExtension.php
@@ -4,6 +4,7 @@ namespace Vivait\WorkerCommandBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -12,7 +13,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class VivaitQueueExtension extends Extension
+class VivaitWorkerCommandExtension extends Extension
 {
     /**
      * {@inheritDoc}
@@ -22,7 +23,7 @@ class VivaitQueueExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ .'/../Resources/config'));
         $loader->load('services.yml');
     }
 }
